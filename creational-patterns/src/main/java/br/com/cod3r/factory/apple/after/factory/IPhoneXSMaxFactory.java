6addeafb21b8ba140239077c14f7d71b518cd491
gabizinha12 <gaibou_lima@hotmail.com>
2023-10-07 01:48:31 -0300

    Create package for after implementation

---
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/after/factory/IPhoneXSMaxFactory.java
@@ -0,0 +1,13 @@
+package br.com.cod3r.factory.apple.after.factory;
+
+import br.com.cod3r.factory.apple.after.model.IPhone;
+import br.com.cod3r.factory.apple.after.model.IPhoneXSMax;
+
+public class IPhoneXSMaxFactory extends IPhoneFactory{
+
+    @Override
+    public IPhone createIPhone() {
+        return new IPhoneXSMax();
+    }
+    
+}
