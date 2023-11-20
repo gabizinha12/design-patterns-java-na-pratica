852b4d4aaa697c30ea896ac17052251f3a7109b3
gabizinha12 <gaibou_lima@hotmail.com>
2023-11-19 23:51:57 -0300

    Implementando db adapter

--- creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/Client.java
@@ -1,8 +1,18 @@
 package br.com.cod3r.factory.app.dbadapter;
 
+import br.com.cod3r.factory.app.dbadapter.db.DB;
+import br.com.cod3r.factory.app.dbadapter.db.PostgresDB;
+import br.com.cod3r.factory.app.dbadapter.factory.OracleDBFactory;
+import br.com.cod3r.factory.app.dbadapter.factory.PostgresDBFactory;
+
 public class Client {
 
 	public static void main(String[] args) {
 
+		DB db = new OracleDBFactory().getDatabase();
+		db.query("SELECT * FROM CLIENTS");
+
+		DB db2 = new PostgresDBFactory().getDatabase();
+		db2.update("Select id from client");
 	}
 }
