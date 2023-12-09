2aabd6efe0b6382497d88c1a7c86d5b3cdf9cbc1
gabizinha12 <gaibou_lima@hotmail.com>
2023-12-09 02:34:56 -0300

    Completed Abstract Factory Apple Implementation

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,24 @@
 package br.com.cod3r.abstractfactory.apple;
 
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
+
 public class Client {
-	
+
 	public static void main(String[] args) {
+		CountryRulesAbstractFactory rules = new BrazilianRulesAbstractFactory();
+		IPhone11Factory gen11Factory = new IPhone11Factory(rules);
+		IPhoneXFactory genXFactory = new IPhoneXFactory(rules);
+
+		System.out.println("### Ordering an IPhone11");
+		IPhone iphone = gen11Factory.orderIPhone("standard");
+		System.out.println(iphone);
+		System.out.println("### Ordering an IPhoneX");
+		IPhone iphoneX = genXFactory.orderIPhone("highEnd");
+		System.out.println(iphoneX);
 
 	}
 }
